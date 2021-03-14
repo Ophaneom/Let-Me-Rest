fe6a50f48e3d506c82769bb8ff902d7e94517d9d
Gustavo Hinkel <78951385+Ophaneom@users.noreply.github.com>
2021-03-14 05:20:49 -0300

    Organized everything.

--- LetMeRest/Data.cs
+++ LetMeRest/Data.cs
@@ -3,11 +3,9 @@
     public class Data
     {
         public float Multiplier { get; set; }
-
         public bool SittingVerification { get; set; }
         public bool RidingVerification { get; set; }
         public bool StandingVerification { get; set; }
-
         public bool EnableSecrets { get; set; }
     }
 }
