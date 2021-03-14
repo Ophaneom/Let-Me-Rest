313a93153e4627a4690a1ecc16ce125b9a229006
Gustavo Hinkel <78951385+Ophaneom@users.noreply.github.com>
2021-03-14 06:20:43 -0300

    Added language support.

--- LetMeRest/Framework/Lists/Buffs.cs
+++ LetMeRest/Framework/Lists/Buffs.cs
@@ -12,8 +12,8 @@ namespace LetMeRest.Framework.Lists
                     Buff restoring_buff = Game1.buffsDisplay.otherBuffs.Find(i => i.source == "LMR_RestoringStamina");
                     if (restoring_buff == null)
                     {
-                        restoring_buff = new Buff("Restoring Stamina.", 0, "LMR_RestoringStamina", 16);
-                        restoring_buff.displaySource = "Relaxing";
+                        restoring_buff = new Buff(ModEntry.instance.Helper.Translation.Get("buff.restoring.description"), 0, "LMR_RestoringStamina", 16);
+                        restoring_buff.displaySource = ModEntry.instance.Helper.Translation.Get("buff.restoring.source");
                         Game1.buffsDisplay.addOtherBuff(restoring_buff);
                     }
                     restoring_buff.millisecondsDuration = 0;
@@ -33,8 +33,8 @@ namespace LetMeRest.Framework.Lists
                     Buff water_buff = Game1.buffsDisplay.otherBuffs.Find(i => i.source == "LMR_Water");
                     if (water_buff == null)
                     {
-                        water_buff = new Buff("Near Water", 0, "LMR_Water", 1);
-                        water_buff.displaySource = "Water";
+                        water_buff = new Buff(ModEntry.instance.Helper.Translation.Get("buff.water.description"), 0, "LMR_Water", 1);
+                        water_buff.displaySource = ModEntry.instance.Helper.Translation.Get("buff.water.source");
                         Game1.buffsDisplay.addOtherBuff(water_buff);
                     }
                     water_buff.millisecondsDuration = 0;
@@ -44,8 +44,8 @@ namespace LetMeRest.Framework.Lists
                     Buff decoration_buff = Game1.buffsDisplay.otherBuffs.Find(i => i.source == "LMR_Decoration");
                     if (decoration_buff == null)
                     {
-                        decoration_buff = new Buff("Nice Decoration!", 0, "LMR_Decoration", 24);
-                        decoration_buff.displaySource = "Decoration";
+                        decoration_buff = new Buff(ModEntry.instance.Helper.Translation.Get("buff.decoration.description"), 0, "LMR_Decoration", 24);
+                        decoration_buff.displaySource = ModEntry.instance.Helper.Translation.Get("buff.decoration.source");
                         Game1.buffsDisplay.addOtherBuff(decoration_buff);
                     }
                     decoration_buff.millisecondsDuration = 0 * 1000;
@@ -55,8 +55,8 @@ namespace LetMeRest.Framework.Lists
                     Buff decoration2_buff = Game1.buffsDisplay.otherBuffs.Find(i => i.source == "LMR_Decoration2");
                     if (decoration2_buff == null)
                     {
-                        decoration2_buff = new Buff("Aweasome place to live!", 0, "LMR_Decoration2", 24);
-                        decoration2_buff.displaySource = "Decoration";
+                        decoration2_buff = new Buff(ModEntry.instance.Helper.Translation.Get("buff.decoration2.description"), 0, "LMR_Decoration2", 24);
+                        decoration2_buff.displaySource = ModEntry.instance.Helper.Translation.Get("buff.decoration2.source");
                         Game1.buffsDisplay.addOtherBuff(decoration2_buff);
                     }
                     decoration2_buff.millisecondsDuration = 0 * 1000;
@@ -66,9 +66,9 @@ namespace LetMeRest.Framework.Lists
                     Buff calm_buff = Game1.buffsDisplay.otherBuffs.Find(i => i.source == "LMR_Calm");
                     if (calm_buff == null)
                     {
-                        calm_buff = new Buff(0, 0, 0, 0, 2, 0, 2, 15, 0, 0, 2, 2, 120 * 1000, "LMR_Calm2", "A calm place");
+                        calm_buff = new Buff(0, 0, 0, 0, 2, 0, 2, 15, 0, 0, 2, 2, 120 * 1000, "LMR_Calm", ModEntry.instance.Helper.Translation.Get("buff.calm.source"));
                         calm_buff.sheetIndex = 21;
-                        calm_buff.description = "So calm...";
+                        calm_buff.description = ModEntry.instance.Helper.Translation.Get("buff.calm.description");
                     }
                     calm_buff.millisecondsDuration = 120 * 1000;
                     break;
@@ -77,9 +77,9 @@ namespace LetMeRest.Framework.Lists
                     Buff calm2_buff = Game1.buffsDisplay.otherBuffs.Find(i => i.source == "LMR_Calm2");
                     if (calm2_buff == null)
                     {
-                        calm2_buff = new Buff(0, 0, 0, 0, 5, 0, 5, 35, 0, 0, 5, 5, 120 * 1000, "LMR_Calm2", "An aweasome location!");
+                        calm2_buff = new Buff(0, 0, 0, 0, 5, 0, 5, 35, 0, 0, 5, 5, 120 * 1000, "LMR_Calm2", ModEntry.instance.Helper.Translation.Get("buff.calm2.source"));
                         calm2_buff.sheetIndex = 21;
-                        calm2_buff.description = "Wow that's so beautiful!";
+                        calm2_buff.description = ModEntry.instance.Helper.Translation.Get("buff.calm2.description");
                         Game1.buffsDisplay.addOtherBuff(calm2_buff);
                     }
                     calm2_buff.millisecondsDuration = 120 * 1000;
